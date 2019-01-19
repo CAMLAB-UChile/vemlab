00960e32343e81155af5ca60ae0bc718aacfd0db
Alejandro Ortiz-Bernardin <aortizb@gmail.com>
2019-01-19 16:09:47 -0300

    Update max_edge_size.m
    correct mesh size calculation

--- vemlab/utilities/geometry/max_edge_size.m
+++ vemlab/utilities/geometry/max_edge_size.m
@@ -1,4 +1,17 @@
 function h_max = max_edge_size(verts)
+
   mysize=size(verts,1);
-  h_max=max(norm(verts-repmat(verts(1,:),mysize,1)));
+  mydist=zeros(mysize,1);
+  
+  % shifts coordinates with respect to the first node in the element connectivity
+  shifted_coords= verts-repmat(verts(1,:),mysize,1);
+  
+  % compute the distances wrt to the first node in the element connectivity
+  for node = 1:mysize
+    mydist(node) = norm(shifted_coords(node,:));
+  end
+  
+  % maximum distance from the first node to any node in the element connectivity 
+  h_max=max(mydist);
+  
 end
