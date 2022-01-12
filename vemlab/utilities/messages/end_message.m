fc11abe00321a812e9552f9d6756cfb05efa5114
Alejandro Ortiz-Bernardin <aortizb@gmail.com>
2022-01-12 17:55:24 -0300

    Updates for VEMLAB 2.4
    Updates for VEMLAB 2.4 (several enhancements to results plotting capabilities, several example test problems added)

--- vemlab/utilities/messages/end_message.m
+++ vemlab/utilities/messages/end_message.m
@@ -2,7 +2,7 @@ function end_message
   fprintf('\n');
   fprintf('Output files are located in folder "test/output_files/"\n\n'); 
   fprintf('For more plotting and output options, setup function "plot_and_output_options.m" located in folder "config"\n\n');   
-  fprintf('For VEMLAB updates visit: http://camlab.cl/research/software/vemlab\n\n');  
+  fprintf('For VEMLAB updates visit: https://camlab.cl/research/software/vemlab\n\n');  
   fprintf('*** VEMLAB has ended ***\n');  
 end
 
